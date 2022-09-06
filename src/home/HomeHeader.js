7094c4aac88aee9d5fc47f0500f5a3b4b99f33ae
Tyler Forbes <forbes2121@gmail.com>
2022-09-05 22:34:32 -0400

    Home and Projects commit

---
+++ src/home/HomeHeader.js
@@ -0,0 +1,7 @@
+export default function HomeHeader() {
+    return (
+        <header className="AppHeader justify-content-center" id="homeHeader">
+            <div className="col" id="cover"></div>
+        </header>
+    )
+}
