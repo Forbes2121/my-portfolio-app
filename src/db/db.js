cfc1ebd08f850c1ef08c8b6d66f955eb47be8b5d
Tyler Forbes <forbes2121@gmail.com>
2022-09-09 19:11:02 -0400

    updated db file

---
+++ src/db/db.js
@@ -0,0 +1,36 @@
+const projects = [
+    {
+        "id": 1,
+        "projectName": "Restaurant Reservation",
+        "readmeUrl": "https://raw.githubusercontent.com/Forbes2121/restaurant-reservation/main/README.md",
+        "github": "https://github.com/Forbes2121/restaurant-reservation",
+        "frontend": true,
+        "backend": true,
+    },
+    {
+        "id": 2,
+        "projectName": "We Love Movies",
+        "readmeUrl": "https://raw.githubusercontent.com/Forbes2121/WeLoveMovies-Capstone/main/README.md",
+        "github": "https://github.com/Forbes2121/WeLoveMovies-Capstone",
+        "frontend": false,
+        "backend": true,
+    },
+    {
+        "id": 3,
+        "projectName": "Flashcards",
+        "readmeUrl": "https://raw.githubusercontent.com/Forbes2121/flashcards-app/main/README.md",
+        "github": "https://github.com/Forbes2121/flashcards-app",
+        "frontend": true,
+        "backend": false,
+    },
+    {
+        "id": 4,
+        "projectName": "Pomodoro",
+        "readmeUrl": "https://raw.githubusercontent.com/Forbes2121/Meme-Generator/main/README.md",
+        "github": "https://github.com/Forbes2121/Meme-Generator",
+        "frontend": true,
+        "backend": false,
+    },
+
+]
+    export default projects;
