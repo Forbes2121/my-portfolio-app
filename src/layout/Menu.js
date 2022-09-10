70b4f97d5899ff2dd1bb8426179768f92081a8bd
Tyler Forbes <forbes2121@gmail.com>
2022-09-09 21:07:33 -0400

    Minor update

--- src/layout/Menu.js
+++ src/layout/Menu.js
@@ -1,23 +1,33 @@
 import { useState, useEffect } from "react";
 import { Link, useHistory } from "react-router-dom";
 
-export default function Menu() {
+export default function Menu(){
+
     const path = useHistory().location.pathname;
 
-    const activeInitiator = {about: "", projects: "", }
+    const activeInitiator = {
+        about: "",
+        projects: "",
+    }
 
     const [active, setActive] = useState(activeInitiator);
 
-    useEffect(() => {
-        if(path === "/about") {
-            setActive({...activeInitiator, about: "active"})
+    useEffect(() =>{
+        if(path === "/about"){
+            setActive({
+                ...activeInitiator,
+                about: "active"
+            })
         }
         if(path === "/projects"){
-            setActive({...activeInitiator, projects: "active"})
+            setActive({
+                ...activeInitiator,
+                projects: "active"
+            })
         }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
-
+    },[])
+    
     return (
         <nav className="row navWrapper" >
             <Link className={`col ${active.about}`} to="/about">
