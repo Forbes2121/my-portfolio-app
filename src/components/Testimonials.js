68be4b8b64055ff7ff96538ee1c3a2e263798e6d
Tyler Forbes <forbes2121@gmail.com>
2022-09-29 08:32:18 -0400

    Update

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { TerminalIcon, UsersIcon } from "@heroicons/react/solid";
+import { TerminalIcon, Heart } from "@heroicons/react/solid";
 import { testimonials } from "../data";
 
 export default function Testimonials() {
   return (
     <section id="testimonials">
       <div className="container px-5 py-10 mx-auto text-center">
-        <UsersIcon className="w-10 inline-block mb-4" />
+        <Heart className="w-10 inline-block mb-4" />
         <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-12">
-          Client Testimonials
+          Outside the Office Activites
         </h1>
         <div className="flex flex-wrap m-4">
           {testimonials.map((testimonial) => (
